87b4a4ca4772becf78b3fd35d220094efc10018b
Alex Knight <alex@alexknight.co.uk>
2023-11-21 22:41:30 +0000

    Added HAProxy & Basic websocket app

--- backend/src/main/scala/uk/co/alexknight/wstest/backend/BackendMain.scala
+++ backend/src/main/scala/uk/co/alexknight/wstest/backend/BackendMain.scala
@@ -1,5 +1,31 @@
-package uk.co.alexknight.wstest.backend
+import zio.*
+import zio.http.*
+import zio.http.ChannelEvent.Read
+import zio.http.codec.PathCodec.string
 
-@main def main(args: String*): Unit = {
-  println("Hello World")
+object BackendMain extends ZIOAppDefault {
+  private val socketApp: WebSocketApp[Any] =
+    Handler.webSocket { channel =>
+      channel.receiveAll {
+        case Read(WebSocketFrame.Text("FOO")) =>
+          channel.send(Read(WebSocketFrame.Text("BAR")))
+        case Read(WebSocketFrame.Text("BAR")) =>
+          channel.send(Read(WebSocketFrame.Text("FOO")))
+        case Read(WebSocketFrame.Text(text)) =>
+          channel.send(Read(WebSocketFrame.Text(text))).repeatN(10)
+        case _ =>
+          ZIO.unit
+      }
+    }
+
+  private val app: HttpApp[Any] =
+    Routes(
+      Method.GET / "greet" / string("name") -> handler {
+        (name: String, req: Request) =>
+          Response.text(s"Greetings {$name}!")
+      },
+      Method.GET / "subscriptions" -> handler(socketApp.toResponse)
+    ).toHttpApp
+
+  override val run = Server.serve(app).provide(Server.defaultWithPort(4003))
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,9 @@ import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
     plugins: [scalaJSPlugin()],
+    server: {
+        host: "127.0.0.1",
+        strictPort: true,
+        port: "5173"
+    }
 });
